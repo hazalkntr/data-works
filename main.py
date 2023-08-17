83f1d1457d26eeff2e41ded92cc92f0f99dd68cc
hazalkntr <hazalkantr@gmail.com>
2023-08-17 14:04:39 +0300

    created the model and predicted

--- main.py
+++ main.py
@@ -29,3 +29,18 @@ print(X)
 
 # print the top few lines
 X.head()
+
+from sklearn.tree import DecisionTreeRegressor
+
+#For model reproducibility, set a numeric value for random_state when specifying the model
+iowa_model = DecisionTreeRegressor(random_state=1)
+
+# Fit the model
+iowa_model.fit(X, y)
+
+print("Making predictions..")
+predictions = iowa_model.predict(X)
+print(predictions)
+
+#compare the top few predictions to the actual home values (in y) for those same homes
+y.head()
