5f043a005807382a7434c0f59140da0e4c436bac
hazalkntr <hazalkantr@gmail.com>
2023-08-18 16:04:58 +0300

    keep all of an artist's songs in one split

--- SpotifySongPopularityDL.py
+++ SpotifySongPopularityDL.py
@@ -30,3 +30,18 @@ preprocessor = make_column_transformer(
     (StandardScaler(), features_num),
     (OneHotEncoder(), features_cat),
 )
+
+def group_split(X, y, group, train_size=0.75):
+    splitter = GroupShuffleSplit(train_size=train_size)
+    train, test = next(splitter.split(X, y, groups=group))
+    return (X.iloc[train], X.iloc[test], y.iloc[train], y.iloc[test])
+
+X_train, X_valid, y_train, y_valid = group_split(X, y, artists)
+
+X_train = preprocessor.fit_transform(X_train)
+X_valid = preprocessor.transform(X_valid)
+y_train = y_train / 100 # popularity is on a scale 0-100, so this rescales to 0-1.
+y_valid = y_valid / 100
+
+input_shape = [X_train.shape[1]]
+print("Input shape: {}".format(input_shape))
