acfb06eb24b84c012196ebfe3001d8679a52f5cb
hazalkntr <hazalkantr@gmail.com>
2023-08-18 16:03:16 +0300

    spotify song popularity project and features added

---
+++ SpotifySongPopularityDL.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-whitegrid')
+
+plt.rc('figure', autolayout=True)
+plt.rc('axes', labelweight='bold', labelsize='large',
+       titleweight='bold', titlesize=18, titlepad=10)
+plt.rc('animation', html='html5')
+
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import make_column_transformer
+from sklearn.model_selection import GroupShuffleSplit
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras import callbacks
+
+spotify = pd.read_csv('~/Downloads/spotify.csv')
+
+X = spotify.copy().dropna()
+y = X.pop('track_popularity')
+artists = X['track_artist']
+
+features_num = ['danceability', 'energy', 'key', 'loudness', 'mode',
+                'speechiness', 'acousticness', 'instrumentalness',
+                'liveness', 'valence', 'tempo', 'duration_ms']
+features_cat = ['playlist_genre']
+
+preprocessor = make_column_transformer(
+    (StandardScaler(), features_num),
+    (OneHotEncoder(), features_cat),
+)
